0a38f8363bf475db846545422c1002764e08e3d0
‘Rustam’ <rustamjonisroilov213@gmail.com>
2025-04-17 10:52:56 +0500

    custom

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,79 @@
-from django.db import models
+from datetime import timezone, datetime, timedelta
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from shared.models import BaseModel
+import uuid
+import random
+
+ORDINARY_USER, MANAGER, ADMIN = ("ordinary", "manager", "admin")
+VIA_EMAIL, VIA_PHONE = ("via_email", 'via_phone')
+NEW, CODE_VERIFIED, DONE, PHOTO_DONE = ('new', "code_verified", "done", "photo_done")
+
+
+class User(AbstractUser, BaseModel):
+    USER_ROLES = (
+        (ORDINARY_USER, ORDINARY_USER),
+        (MANAGER, MANAGER),
+        (ADMIN, ADMIN)
+    )
+    AUTH_TYPE_CHOICES = (
+        (VIA_EMAIL, VIA_EMAIL),
+        (VIA_PHONE, VIA_PHONE),
+    )
+    AUTH_STATUS = (
+        (NEW, NEW),
+        (CODE_VERIFIED, CODE_VERIFIED),
+        (DONE, DONE),
+        (PHOTO_DONE, PHOTO_DONE),
+    )
+    id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False, primary_key=True)
+    user_role = models.CharField(max_length=31, choices=USER_ROLES, default=ORDINARY_USER)
+    auth_type = models.CharField(max_length=31, choices=AUTH_TYPE_CHOICES)
+    auth_status = models.CharField(max_length=31, choices=AUTH_STATUS, default=NEW)
+    email = models.EmailField(null=True, blank=True, unique=True)
+    phone_number = models.CharField(max_length=31, null=True, blank=True, unique=True)
+    photo = models.ImageField(null=True, blank=True, upload_to="user_photos/")
+
+    def __str__(self):
+        return self.phone_number
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def create_verify_code(self, verify_type):
+        code = ''.join([str(random.randint(0, 10000) % 10) for _ in range(4)])  # ["7", "5", "3", "9"] = 7539
+        UserConfirmation.objects.create(
+            user_id=self.id,
+            verify_type=verify_type,
+            code=code,
+        )
+        return code
+
+
+
+PHONE_EXPIRE = 2
+EMAIL_EXPIRE = 5
+
+
+class UserConfirmation(BaseModel):
+    TYPE_CHOICES = (
+        (VIA_EMAIL, VIA_EMAIL),
+        (VIA_PHONE, VIA_PHONE),
+    )
+    code = models.CharField(max_length=4)
+    verify_type = models.CharField(max_length=31, choices=TYPE_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="verify_codes")
+    expiration_time = models.DateTimeField(null=True, blank=True)
+    is_confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user.__str__())
+
+    def save(self, *args, **kwargs):
+        if self.verify_type == VIA_EMAIL:
+            self.expiration_time = timezone.now() + timedelta(minutes=EMAIL_EXPIRE)
+        else:
+            self.expiration_time = timezone.now() + timedelta(minutes=PHONE_EXPIRE)
+        super(UserConfirmation, self).save(*args, **kwargs)
